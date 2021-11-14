898fdee0b54933e1d8a105453f1304184f6df70e
GefeiTan <tangefei1998@163.com>
2021-11-14 17:23:08 -0600

    Fixed pre-req

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,15 +1,22 @@
 import {useState} from 'react';
 import {getStartingCourses} from '../utils/coursesUtils';
 import {Courses} from './Courses';
+import {useEffect} from "react";
 
 
 export const CourseList = () => {
+
+
     const courses = require('../files/toy_courses.json');
     const coursesArr = Object.entries(courses);
 
     const [canTake, setCanTake] = useState(getStartingCourses(coursesArr));
     const [selected, setSelected] = useState([]);
 
+    useEffect(() => {
+        checkCanTakeCourses(coursesArr, selected, canTake, setCanTake);
+    }, [selected]);
+    // console.log(canTake);
     return (
 
         <>
@@ -43,26 +50,39 @@ const Course = ({coursesArr, course, setSelected, selected, canTake, setCanTake}
 }
 const take = (coursesArr, course, setSelected, selected, canTake, setCanTake) => {
     selected.includes(course) ? setSelected(selected.filter((element) => element[1].course_name !== course[1].course_name)) : selected ? setSelected([...selected, course]) : setSelected([course]);
-    checkCanTakeCourses(coursesArr, selected, canTake, setCanTake);
+    // checkCanTakeCourses(coursesArr, selected, canTake, setCanTake);
 }
 
 const checkCanTakeCourses = (coursesArr, selected, canTake, setCanTake) => {
     coursesArr.forEach((course) => {
         if (ifPreMet(course, selected)) {
-            setCanTake([...canTake, course]);
+            let skip = false;
+            console.log("course: ", course);
+            console.log("canTake: ", canTake);
+            console.log("in array", canTake.includes(course));
+            // horrible way to check if it's in array
+            canTake.forEach((ele) => {
+                if (ele[0] === course[0]) {
+                    skip = true;
+                }
+            })
+            if (!skip) {
+                setCanTake([...canTake, course]);
+            }
         }
     })
 }
 
 const ifPreMet = (course, selected) => {
+    // console.log(selected);
     let preArr = course[1].Prereqs;
     selected.forEach((selectedCourse) => {
-        console.log(preArr);
+        // console.log(preArr);
         preArr = preArr.filter((singlePreArr) => !singlePreArr.includes(selectedCourse[0]));
     });
 
 
-    console.log(preArr.length === 0);
+    // console.log(preArr.length === 0);
     return preArr.length === 0;
 }
 export default CourseList;
