12a7da04564759e534b517f9bd456bda39694c25
QiuOOGan <ganqiu@ucdavis.edu>
2021-11-14 15:51:09 -0600

    select and deselect course

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -7,26 +7,38 @@ export const CourseList = () => {
     const courses = require('../files/toy_courses.json');
     const coursesArr = Object.entries(courses);
 
-    const [taken, setTaken] = useState(getStartingCourses(coursesArr));
+    const [canTake, setCanTake] = useState(getStartingCourses(coursesArr));
+    const [selected, setSelected] = useState([]);
 
     return (
-        <div className="course-list">
-            {taken.map( (course) => <Course course = {course} ></Course>)}
-        </div>
+
+        <>
+            <div className="course-list">
+                {canTake.map( (course) => <Course course = {course} setSelected={setSelected} selected={selected} ></Course>)}
+            </div>
+
+            <div className="course-list">
+                {selected.map((ele) => <div className="card m-1 p-2"> {ele[1].course_name} </div>)}
+            </div>
+
+        </>
     )
 }
 
-const Course = ({course}) => {
+const Course = ({course, setSelected, selected}) => {
 
     if (!course){
         return null;
     }
 
     return (
-        <div className="card m-1 p-2">
+        <div className="card m-1 p-2" onClick={() => take(course, setSelected, selected)}>
             {course[0]} : {course[1].course_name}
         </div>
     );
+}
+const take = (course, setSelected, selected) => {
+    selected.includes(course) ? setSelected(selected.filter((element) => element[1].course_name !== course[1].course_name)) : selected ? setSelected([...selected, course]) : setSelected([course]);
 }
 
 export default CourseList;
