9d52fc882459cf3a8d3f0d7146f62c4968339243
QiuOOGan <ganqiu@ucdavis.edu>
2021-11-14 21:09:52 -0600

    Add comments so I can understand it tomorrow

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import {CourseList} from './components/CourseList';
 //import { useData } from './utilities/firebase.js';
 
-const Banner = ({ title }) => (
-  <h1>{ title }</h1>
-);
+// const Banner = ({ title }) => (
+//   <h1>{ title }</h1>
+// );
 
 const App = () => {
   //const [schedule, loading, error] = useData('/', addScheduleTimes); 
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,8 +1,5 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {getStartingCourses} from '../utils/coursesUtils';
-
-
-
 
 export const CourseList = () => {
 
@@ -34,14 +31,12 @@ export const CourseList = () => {
 }
 
 const SelectedCourse = ({coursesArr, course, setSelected, selected, canTake, setCanTake}) => {
-    if (!course) {
-        return null;
-    }
-
     return (
-        <div className="card m-1 p-2" onClick={() => {detake(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
-            {course[0]} : {course[1].course_name}
-        </div>
+        course ? 
+            <div className="card m-1 p-2" onClick={() => {detake(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
+                {course[0]} : {course[1].course_name}
+            </div>
+        : null
     );
 
 }
@@ -53,46 +48,58 @@ const CanTakeCourse = ({coursesArr, course, setSelected, selected, canTake, setC
     }
 
     return (
-        <div className="card m-1 p-2" onClick={() => {take(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
-            {course[0]} : {course[1].course_name}
-        </div>
+        course ?
+            <div className="card m-1 p-2" onClick={() => {take(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
+                {course[0]} : {course[1].course_name}
+            </div>
+            : null
     );
 }
 const take = (coursesArr, course, setSelected, selected, canTake, setCanTake) => {
 
     // Add this course to Selected
+    // Note: Create a new varialbe first, so it's to avoid asynchrounous effect.
     let newSelected = selected ? [...selected, course] : [course];
     setSelected(newSelected);
 
+    // Note: Create a new varialbe first, so it's to avoid asynchrounous effect.
     let newCanTake = canTake.filter((singleCanTake) => course[1].course_name !== singleCanTake[1].course_name)
-
-    console.log("canTake: ", checkCanTakeCourses(coursesArr, newSelected, newCanTake, setCanTake));
     setCanTake(checkCanTakeCourses(coursesArr, newSelected, newCanTake, setCanTake));
 }
 
 const detake = (coursesArr, course, setSelected, selected, canTake, setCanTake) => {
 
-    let selectedNew = selected.filter((singleSelected) => course[1].course_name !== singleSelected[1].course_name)
-    let selectedNewNew = removeGuiltyCourses(selectedNew, canTake)
-    setSelected(selectedNewNew);
-    setCanTake(checkCanTakeCourses(coursesArr, selectedNewNew));
-    console.log("selected: ", removeGuiltyCourses(selectedNew))
+    // Remove this course from selected courses. Create Variable first to avoid asynchrounous effect.
+    let selectedNew = removeGuiltyCourses(selected.filter((singleSelected) => course[1].course_name !== singleSelected[1].course_name), canTake)
+    setSelected(selectedNew);
+
+    // Update CanTake courses becauase we just deselect a course.
+    setCanTake(checkCanTakeCourses(coursesArr, selectedNew));
 }
 
 // Recursively Remove Unsatisfiable courses
-const removeGuiltyCourses = (selected, canTake) => {
+const removeGuiltyCourses = (selected) => {
     let filtered = selected.filter((single) => ifPreMet(single, selected));
+
+    // if filtered's and selected's lengths are equal, end of recursion
+    // else: continue removing courses
     return filtered.length === selected.length ? filtered : removeGuiltyCourses(filtered);
 }
 
-const checkCanTakeCourses = (coursesArr, selected) => {
-    console.log("selected: ", selected);
-    return coursesArr.filter((course) => ifPreMet(course, selected) && !courseSelectedAlready(course, selected));
-}
+// get canTake courses from all courses based on selected courses. 
+const checkCanTakeCourses = (coursesArr, selected) => (
 
-const courseSelectedAlready = (course, selected) => {
-    return selected.some((singleSelected) => singleSelected[1].course_name === course[1].course_name);
-}
+    // If:
+    //    1: Prerequistes met
+    //    2: this courses hasn't been selected yet
+    coursesArr.filter((course) => ifPreMet(course, selected) && !courseSelectedAlready(course, selected))
+);
+
+const courseSelectedAlready = (course, selected) => (
+
+    // Check if course is in selected
+    selected.some((singleSelected) => singleSelected[1].course_name === course[1].course_name)
+);
 
 const ifPreMet = (course, selected) => {
     let preArr = course[1].Prereqs;
