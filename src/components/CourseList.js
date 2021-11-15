180d3c8584c4e3900f9c2fd0a5e5ca2dd0e103b1
QiuOOGan <ganqiu@ucdavis.edu>
2021-11-14 20:52:58 -0600

    refactored

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,11 +1,10 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {getStartingCourses} from '../utils/coursesUtils';
-import {Courses} from './Courses';
-import {useEffect} from "react";
+
+
 
 
 export const CourseList = () => {
-
 
     const courses = require('../files/toy_courses.json');
     const coursesArr = Object.entries(courses);
@@ -13,76 +12,93 @@ export const CourseList = () => {
     const [canTake, setCanTake] = useState(getStartingCourses(coursesArr));
     const [selected, setSelected] = useState([]);
 
-    useEffect(() => {
-        checkCanTakeCourses(coursesArr, selected, canTake, setCanTake);
-    }, [selected]);
-    // console.log(canTake);
     return (
 
         <>
+            <h1> Selected: </h1>
             <div className="course-list">
-                {canTake.map((course) => <Course course={course} setSelected={setSelected}
+                {selected.map((course) => <SelectedCourse course={course} setSelected={setSelected}
                                                  selected={selected} coursesArr={coursesArr} canTake={canTake}
                                                  setCanTake={setCanTake}/>)}
             </div>
 
-            <div className="course-list">
-                {selected.map((ele) => <div className="card m-1 p-2"> {ele[1].course_name} </div>)}
-            </div>
 
+            <h1> Can Take: </h1>
+            <div className="course-list">
+                {canTake.map((course) => <CanTakeCourse course={course} setSelected={setSelected}
+                                                 selected={selected} coursesArr={coursesArr} canTake={canTake}
+                                                 setCanTake={setCanTake}/>)}
+            </div>
         </>
     )
 }
 
-const Course = ({coursesArr, course, setSelected, selected, canTake, setCanTake}) => {
+const SelectedCourse = ({coursesArr, course, setSelected, selected, canTake, setCanTake}) => {
+    if (!course) {
+        return null;
+    }
+
+    return (
+        <div className="card m-1 p-2" onClick={() => {detake(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
+            {course[0]} : {course[1].course_name}
+        </div>
+    );
+
+}
+
+const CanTakeCourse = ({coursesArr, course, setSelected, selected, canTake, setCanTake}) => {
 
     if (!course) {
         return null;
     }
 
     return (
-        <div className="card m-1 p-2" onClick={() => {
-            take(coursesArr, course, setSelected, selected, canTake, setCanTake);
-        }}>
+        <div className="card m-1 p-2" onClick={() => {take(coursesArr, course, setSelected, selected, canTake, setCanTake);}}>
             {course[0]} : {course[1].course_name}
         </div>
     );
 }
 const take = (coursesArr, course, setSelected, selected, canTake, setCanTake) => {
-    selected.includes(course) ? setSelected(selected.filter((element) => element[1].course_name !== course[1].course_name)) : selected ? setSelected([...selected, course]) : setSelected([course]);
-    // checkCanTakeCourses(coursesArr, selected, canTake, setCanTake);
+
+    // Add this course to Selected
+    let newSelected = selected ? [...selected, course] : [course];
+    setSelected(newSelected);
+
+    let newCanTake = canTake.filter((singleCanTake) => course[1].course_name !== singleCanTake[1].course_name)
+
+    console.log("canTake: ", checkCanTakeCourses(coursesArr, newSelected, newCanTake, setCanTake));
+    setCanTake(checkCanTakeCourses(coursesArr, newSelected, newCanTake, setCanTake));
 }
 
-const checkCanTakeCourses = (coursesArr, selected, canTake, setCanTake) => {
-    coursesArr.forEach((course) => {
-        if (ifPreMet(course, selected)) {
-            let skip = false;
-            console.log("course: ", course);
-            console.log("canTake: ", canTake);
-            console.log("in array", canTake.includes(course));
-            // horrible way to check if it's in array
-            canTake.forEach((ele) => {
-                if (ele[0] === course[0]) {
-                    skip = true;
-                }
-            })
-            if (!skip) {
-                setCanTake([...canTake, course]);
-            }
-        }
-    })
+const detake = (coursesArr, course, setSelected, selected, canTake, setCanTake) => {
+
+    let selectedNew = selected.filter((singleSelected) => course[1].course_name !== singleSelected[1].course_name)
+    let selectedNewNew = removeGuiltyCourses(selectedNew, canTake)
+    setSelected(selectedNewNew);
+    setCanTake(checkCanTakeCourses(coursesArr, selectedNewNew));
+    console.log("selected: ", removeGuiltyCourses(selectedNew))
+}
+
+// Recursively Remove Unsatisfiable courses
+const removeGuiltyCourses = (selected, canTake) => {
+    let filtered = selected.filter((single) => ifPreMet(single, selected));
+    return filtered.length === selected.length ? filtered : removeGuiltyCourses(filtered);
+}
+
+const checkCanTakeCourses = (coursesArr, selected) => {
+    console.log("selected: ", selected);
+    return coursesArr.filter((course) => ifPreMet(course, selected) && !courseSelectedAlready(course, selected));
+}
+
+const courseSelectedAlready = (course, selected) => {
+    return selected.some((singleSelected) => singleSelected[1].course_name === course[1].course_name);
 }
 
 const ifPreMet = (course, selected) => {
-    // console.log(selected);
     let preArr = course[1].Prereqs;
     selected.forEach((selectedCourse) => {
-        // console.log(preArr);
         preArr = preArr.filter((singlePreArr) => !singlePreArr.includes(selectedCourse[0]));
     });
-
-
-    // console.log(preArr.length === 0);
     return preArr.length === 0;
 }
 export default CourseList;
