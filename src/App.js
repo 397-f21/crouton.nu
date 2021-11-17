e428f73b91bd99673dda3505d3edb7eba7545472
rgoodman22 <goodman.rob24@gmail.com>
2021-11-16 21:55:11 -0600

    full team - changing tool selector and styling, updating prereq for CS 351-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {CourseList} from './components/CourseList';
+import { CourseList } from './components/CourseList';
 import PathRecom from './components/PathRecom';
-import TermSelector from './components/ToolSelector';
+import ToolSelector from './components/ToolSelector';
 import AllCourses from "./components/AllCourses";
 //import { useData } from './utilities/firebase.js';
 
@@ -14,14 +14,14 @@ const App = () => {
         <div className="container">
             <div>
                 <img className="logo"
-                     src="https://firebasestorage.googleapis.com/v0/b/cloud-walker-c72ce.appspot.com/o/logos%2FcroutonLogo.001.jpeg?alt=media&token=04edf314-5737-400d-801c-0c95f5f2fd81"
-                     alt="new">
+                    src="https://firebasestorage.googleapis.com/v0/b/cloud-walker-c72ce.appspot.com/o/logos%2FcroutonLogo.001.jpeg?alt=media&token=04edf314-5737-400d-801c-0c95f5f2fd81"
+                    alt="new">
                 </img>
             </div>
-            <TermSelector setTool={setTool}/>
-            {tool === "Course Explorer" ? <CourseList/> : null}
-            {tool === "Path Recommendation" ? <PathRecom/> : null}
-            {tool === "All Courses" ? <AllCourses/> : null}
+            <ToolSelector setTool={setTool} toolState={tool} />
+            {tool === "Course Explorer" ? <CourseList /> : null}
+            {tool === "Path Recommendation" ? <PathRecom /> : null}
+            {tool === "All Courses" ? <AllCourses /> : null}
         </div>
     );
 };
--- src/components/ToolSelector.js
+++ src/components/ToolSelector.js
@@ -1,24 +1,24 @@
 
 
-const TermSelector = ({setTool}) => {
+const ToolSelector = ({ setTool, toolState }) => {
 
     const tools = ["Course Explorer", "Path Recommendation", "All Courses"];
     return (
         <div className="btn-group">
-        { 
-            tools.map(tool => <ToolButton key={tool} tool={tool} setTool={setTool}/>)
-        }
+            {
+                tools.map(tool => <ToolButton key={tool} tool={tool} setTool={setTool} toolState={toolState} />)
+            }
         </div>
     )
 };
 
 
-const ToolButton = ({tool, setTool}) => (
-    <>
-      <button className="btn btn-outline-secondary btn-sm m-1" onClick={() => setTool(tool)}>
-                {tool}
-      </button>
-    </>
-);
-
-export default TermSelector;
+const ToolButton = ({ tool, setTool, toolState }) => {
+    const classes = 'btn btn-outline-secondary btn-sm m-1' + (toolState === tool ? ' active' : '');
+    return (
+        <button className={classes} onClick={() => setTool(tool)}>
+            {tool}
+        </button>
+    )
+}
+export default ToolSelector;
