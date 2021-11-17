fcbb4b7f6a1a0c1b15bb4eb33026d2febca27c03
rgoodman22 <goodman.rob24@gmail.com>
2021-11-16 21:45:06 -0600

    robbie, fionna, gefei - styling accordion

--- src/components/AllCourses.js
+++ src/components/AllCourses.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Accordion} from "react-bootstrap";
+import { Accordion } from "react-bootstrap";
 
-const {useState} = require('react');
+const { useState } = require('react');
 
 const AllCourses = () => {
     const courses = require('../files/toy_courses.json');
@@ -9,10 +9,10 @@ const AllCourses = () => {
 
     return (
         <>
-            <hr/>
+            <hr />
             <h1> All Courses: </h1>
             <div className="course-list">
-                {coursesArr.map((course) => <DetailedCourses course={course}/>)}
+                {coursesArr.map((course) => <DetailedCourses course={course} />)}
             </div>
         </>
     )
@@ -20,49 +20,48 @@ const AllCourses = () => {
 
 }
 
-const DetailedCourses = ({course}) => {
+const DetailedCourses = ({ course }) => {
 
     if (!course) {
         return;
     }
     let courseNo = course[0].split(" ")[1];
     let link = `https://www.mccormick.northwestern.edu/computer-science/academics/courses/descriptions/${courseNo}.html`;
-    // TODO Fix the ugliness
-    // return (
-    //     <div className="card m-1 p-2">
-    //         {course[0]} : {course[1].course_name}
-    //         <br/>
-    //         {course[1].fall_time ? "\nFall: " + course[1].fall_time + ' \n' + course[1].fall_instructor : ""}
-    //         {course[1].winter_time ? "\nWinter: " + course[1].winter_time + ' \n' + course[1].winter_instructor : ""}
-    //         {course[1].spring_time ? "\nSpring: " + course[1].spring_time + ' \n' + course[1].spring_instructor : ""}
-    //         {course[1].other.Prerequisites ? "\nPrerequisites: " + course[1].other.Prerequisites : '\nPrerequisites: None'}
-    //         <button className="btn btn-outline-primary btn-sm m-1" onClick={() => window.open(link, '_blank')}>
-    //             Course Detail
-    //         </button>
-    //     </div>
-    //
-    //
-    // );
-
     return (
-
         <Accordion className="m-1 p-2">
             <Accordion.Item eventKey="0">
                 <Accordion.Header>{course[0]} : {course[1].course_name}</Accordion.Header>
                 <Accordion.Body>
-                    {course[1].fall_time ? "\nFall: " + course[1].fall_time + ' \n' + course[1].fall_instructor : ""}
-                    {course[1].winter_time ? "\nWinter: " + course[1].winter_time + ' \n' + course[1].winter_instructor : ""}
-                    {course[1].spring_time ? "\nSpring: " + course[1].spring_time + ' \n' + course[1].spring_instructor : ""}
-                    {course[1].other.Prerequisites ? "\nPrerequisites: " + course[1].other.Prerequisites : '\nPrerequisites: None'}
-                    <button className="btn btn-outline-primary btn-sm m-1"
-                            onClick={() => window.open(link, '_blank')}>
-                        Course Detail
-                    </button>
+                    <CourseBody course={course} link={link} />
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
-
     );
+}
+
+const CourseBody = ({ course, link }) => {
+    const fall = course[1].fall_time ? course[1].fall_time + ' \n' + course[1].fall_instructor : "";
+    const winter = course[1].winter_time ? course[1].winter_time + ' \n' + course[1].winter_instructor : "";
+    const spring = course[1].spring_time ? course[1].spring_time + ' \n' + course[1].spring_instructor : "";
+    const prereq = course[1].other.Prerequisites ? course[1].other.Prerequisites : 'None';
+    const fallLineBreak = course[1].fall_time ? <br /> : "";
+    const winterLineBreak = course[1].winter_time ? <br /> : "";
+    const springLineBreak = course[1].spring_time ? <br /> : "";
+    const prereqLineBreak = course[1].other.Prerequisites ? <br /> : "";
+    const button = <button className="btn btn-outline-primary btn-sm m-1" onClick={() => window.open(link, '_blank')}> Course Detail </button>;
+    const fallTitle = course[1].fall_time ? <strong>Fall:</strong> : "";
+    const winterTitle = course[1].winter_time ? <strong>Winter:</strong> : "";
+    const springTitle = course[1].spring_time ? <strong>Spring:</strong> : "";
+    const prereqTitle = <strong>Prerequisites:</strong>;
+    return (
+        <>
+            {fallTitle} {fall} {fallLineBreak}
+            {winterTitle} {winter} {winterLineBreak}
+            {springTitle} {spring} {springLineBreak}
+            {prereqTitle} {prereq} {prereqLineBreak}
+            {button}
+        </>
+    )
 }
 
 
